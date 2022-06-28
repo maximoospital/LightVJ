bcdc9eff7fc414fc6be17912068b4492376e7702
maximoospital <59743027+maximoospital@users.noreply.github.com>
2022-06-28 19:22:16 -0300

    initial build

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Test")
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout
+from qt_material import apply_stylesheet
+from qt_material import list_themes
+app = QApplication([])
+window = QWidget()
+apply_stylesheet(app, theme='dark_red.xml')
+layout = QVBoxLayout()
+layout.addWidget(QPushButton('Top'))
+layout.addWidget(QPushButton('Bottom'))
+window.setLayout(layout)
+window.show()
+app.exec()
