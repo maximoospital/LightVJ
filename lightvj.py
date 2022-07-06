33240e6e1f72844835446be7770048b14f202684
maximoospital <59743027+maximoospital@users.noreply.github.com>
2022-07-06 17:10:36 -0300

    settings menu

--- lightvj.py
+++ lightvj.py
@@ -1,12 +1,18 @@
 import webbrowser
+from PyQt5.QtCore import Qt
 from PyQt5 import QtGui, QtWidgets
 from PyQt5.QtWidgets import (QApplication)
 from qt_material import apply_stylesheet
+from screens.settings import Settings
 from screens.about import About
 from screens.MainWidget import mainWidget
 
+QtWidgets.QApplication.setAttribute(Qt.AA_EnableHighDpiScaling, True)
+
+
 # Main window
 class MainWindow(QtWidgets.QMainWindow):
+    # menu bar creation
     def _createMenuBar(self):
         # filling up a menu bar
         bar = self.menuBar()
@@ -22,17 +28,11 @@ class MainWindow(QtWidgets.QMainWindow):
         file_menu.addAction(saveas_action)
         file_menu.addAction(close_action)
         # Edit menu
-        settings_menu = bar.addMenu('Settings')
+        edit_menu = bar.addMenu('Edit')
         # adding actions to edit menu
-        gen_action = QtWidgets.QAction('General Settings', self)
-        output_action = QtWidgets.QAction('Output Settings', self)
-        link_action = QtWidgets.QAction('Ableton Link Settings', self)
-        spout_action = QtWidgets.QAction('Spout Settings', self)
-        settings_menu.addAction(gen_action)
-        settings_menu.addAction(output_action)
-        settings_menu.addAction(link_action)
-        settings_menu.addAction(spout_action)
-        # Edit menu
+        set_action = QtWidgets.QAction('Settings', self)
+        edit_menu.addAction(set_action)
+        # Help menu
         help_menu = bar.addMenu('Help')
         # adding actions to edit menu
         update_action = QtWidgets.QAction('Check for Updates', self)
@@ -46,17 +46,25 @@ class MainWindow(QtWidgets.QMainWindow):
 
         # use `connect` method to bind signals to desired behavior
         close_action.triggered.connect(self.close)
+        set_action.triggered.connect(self.set_clicked)
         gh_action.triggered.connect(lambda: webbrowser.open('https://github.com/maximoospital'))
         about_action.triggered.connect(self.about_clicked)
 
+    # settings button functionality
+    def set_clicked(self, s):
+        dlg = Settings(self)
+        button = dlg.exec()
+
+    # about button functionality
     def about_clicked(self, s):
         dlg = About(self)
         button = dlg.exec()
 
     def __init__(self, parent=None):
         super().__init__(parent)
+        # title, icon and size definition
         self.setWindowTitle("LightVJ")
-        self.setWindowIcon(QtGui.QIcon('resources\lvjicon.png'))
+        self.setWindowIcon(QtGui.QIcon('resources/lvjicon.png'))
         self.mainWidget = mainWidget(parent=self)
         self.setCentralWidget(self.mainWidget)
         self._createMenuBar()
@@ -67,6 +75,7 @@ if __name__ == '__main__':
     import sys
 
     app = QApplication(sys.argv)
+    # apply material style and show window
     apply_stylesheet(app, theme='dark_red.xml')
 
     main = MainWindow()
